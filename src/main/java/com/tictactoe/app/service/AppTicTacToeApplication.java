553c7987cbb50a9214a69c86dbfe4b670cf8fad6
Parnika <govind24n@gmail.com>
2022-07-29 22:21:48 +0530

    Initial commit

---
+++ src/main/java/com/tictactoe/app/service/AppTicTacToeApplication.java
@@ -0,0 +1,13 @@
+package com.tictactoe.app.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTicTacToeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTicTacToeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tictactoe/app/service/AppTicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tictactoe.app.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
