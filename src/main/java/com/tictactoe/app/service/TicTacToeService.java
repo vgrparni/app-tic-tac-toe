07f9cd40dc42b8efb5fb5dfaceb21f614e084665
Parnika <govind24n@gmail.com>
2022-07-30 14:59:11 +0530

    Implemented feature to player winning harizontally.

--- src/main/java/com/tictactoe/app/service/TicTacToeService.java
+++ src/main/java/com/tictactoe/app/service/TicTacToeService.java
@@ -1,8 +1,22 @@
 package com.tictactoe.app.service;
 
-public class TicTacToeService {
+import java.util.Arrays;
+import java.util.Scanner;
 
-	private String[] boardOfGame = new String[9];
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class TicTacToeService {
+	 private static final Logger log = LoggerFactory.getLogger(TicTacToeService.class);
+
+    private String[] boardOfGame = new String[9];
+	private String turn = "X";
+	private String playerStatus;
+
+	public String getPlayerStatus() {
+		return playerStatus;
+	}
 
 	public void loadDefaultBoard() {
 		for (int a = 0; a < 9; a++) {
@@ -11,10 +25,77 @@ public class TicTacToeService {
 	}
 
 	public String getDisplayBoard() {
+		
 		String createBoard = "/---|---|---\\ \n" + "| " + boardOfGame[0] + " | " + boardOfGame[1] + " | "
 				+ boardOfGame[2] + " | \n" + "|-----------| \n" + "| " + boardOfGame[3] + " | " + boardOfGame[4] + " | "
 				+ boardOfGame[5] + " | \n" + "|-----------| \n" + "| " + boardOfGame[6] + " | " + boardOfGame[7] + " | "
 				+ boardOfGame[8] + " | \n" + "/---|---|---\\ \n";
+		log.info("Game board : "+createBoard);
 		return createBoard;
 	}
+
+	public void runGame() {
+		log.info("*********Game started!*********");
+		String winner = "";
+		loadDefaultBoard();
+		log.info("Welcoming to 2 Players(X and O) to play Tic Tac Toe.");
+
+		getDisplayBoard();
+		log.info("X's will play first. Enter a slot number to place X in:");
+
+		try (Scanner in = new Scanner(System.in)) {
+			while ( winner.isEmpty()) {
+				int numInput = in.nextInt();
+				if (boardOfGame[numInput - 1].equals(String.valueOf(numInput))) {
+					boardOfGame[numInput - 1] = turn;
+					if (turn.equals("X")) {
+						turn = "O";
+					}else {
+						turn = "X";
+					}
+					getDisplayBoard();
+					winner = checkWinner();
+				}
+			}
+			if (!winner.isEmpty()) {
+				playerStatus = "Congratulations! " + winner + "'s have won the Game";
+				log.info("Congratulations! " + winner + "'s have won! Thanks for playing.");
+			}
+		}
+		log.info("*********Game Finished!*********");
+	}
+
+	/**
+	 * This method used to check the winner as per the move
+	 * @return
+	 */
+	public String checkWinner() {
+		for (int a = 0; a < 3; a++) {
+			String line = "";
+			switch (a) {
+			case 0:
+				line = boardOfGame[0] + boardOfGame[1] + boardOfGame[2];
+				break;
+			case 1:
+				line = boardOfGame[3] + boardOfGame[4] + boardOfGame[5];
+				break;
+			case 2:
+				line = boardOfGame[6] + boardOfGame[7] + boardOfGame[8];
+				break;
+			}
+			if (line.equals("XXX")) {
+				return "X";
+			} 
+		}
+
+		for (int a = 0; a < 9; a++) {
+			if (Arrays.asList(boardOfGame).contains(String.valueOf(a + 1))) {
+				break;
+			}
+		}
+
+		log.info(turn + "'s turn; enter a slot number to place " + turn + " in:");
+		return "";
+	}
+
 }
--- src/test/java/com/tictactoe/app/service/TicTacToeServiceTest.java
+++ src/test/java/com/tictactoe/app/service/TicTacToeServiceTest.java
@@ -2,7 +2,14 @@ package com.tictactoe.app.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,11 +19,28 @@ public class TicTacToeServiceTest {
 	@Spy
 	private TicTacToeService ticTacToeService;
 
+	@BeforeEach
+	public void beforeEach()
+	{
+		MockitoAnnotations.openMocks(this);
+	}
+	
 	@Test
 	public void displayDefaultGameBoard() {
 		String expectedResult = "/---|---|---\\ \n" + "| 1 | 2 | 3 | \n" + "|-----------| \n" + "| 4 | 5 | 6 | \n"
 				+ "|-----------| \n" + "| 7 | 8 | 9 | \n" + "/---|---|---\\ \n";
 		ticTacToeService.loadDefaultBoard();
 		assertEquals(expectedResult, ticTacToeService.getDisplayBoard());
+	}
+	
+	@Test
+	public void checkthePlayerWinningHarizontal() {
+		List<String> playersMoves = Arrays.asList("1","4","2","5","3","6","7","8","9");
+        String delim = System.getProperty("line.separator");
+        String playerMovesTogether = String.join(delim, playersMoves);
+	    InputStream in = new ByteArrayInputStream(playerMovesTogether.getBytes());
+	    System.setIn(in);
+	    ticTacToeService.runGame();
+	    assertEquals("Congratulations! X's have won the Game", ticTacToeService.getPlayerStatus());
 	}
 }
