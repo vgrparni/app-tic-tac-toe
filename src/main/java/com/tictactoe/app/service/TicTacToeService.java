dfede7f73d6ea6710d7bc6981e9d8b1d314c091c
Parnika <govind24n@gmail.com>
2022-08-01 18:22:59 +0530

    Varialbles renamed, declared constants for repetitive fixed values.

--- src/main/java/com/tictactoe/app/service/TicTacToeService.java
+++ src/main/java/com/tictactoe/app/service/TicTacToeService.java
@@ -10,8 +10,11 @@ import org.slf4j.LoggerFactory;
 public class TicTacToeService {
 	private static final Logger log = LoggerFactory.getLogger(TicTacToeService.class);
 
-	private String[] boardOfGame = new String[9];
-	private String turnOfPlayer = "X";
+	private final String[] squareBoard = new String[9];
+	private final static String  PLAYER_X = "X";
+	private final static String  PLAYER_O = "O";
+	private String turnOfPlayer = PLAYER_X;
+	private final static String STATUS_DRAW = "draw";
 	private String playerStatus;
 
 	public String getPlayerStatus() {
@@ -20,16 +23,16 @@ public class TicTacToeService {
 
 	public void loadDefaultBoard() {
 		for (int a = 0; a < 9; a++) {
-			boardOfGame[a] = String.valueOf(a + 1);
+			squareBoard[a] = String.valueOf(a + 1);
 		}
 	}
 
 	public String getDisplayBoard() {
 
-		String createBoard = "/---|---|---\\ \n" + "| " + boardOfGame[0] + " | " + boardOfGame[1] + " | "
-				+ boardOfGame[2] + " | \n" + "|-----------| \n" + "| " + boardOfGame[3] + " | " + boardOfGame[4] + " | "
-				+ boardOfGame[5] + " | \n" + "|-----------| \n" + "| " + boardOfGame[6] + " | " + boardOfGame[7] + " | "
-				+ boardOfGame[8] + " | \n" + "/---|---|---\\ \n";
+		String createBoard = "/---|---|---\\ \n" + "| " + squareBoard[0] + " | " + squareBoard[1] + " | "
+				+ squareBoard[2] + " | \n" + "|-----------| \n" + "| " + squareBoard[3] + " | " + squareBoard[4] + " | "
+				+ squareBoard[5] + " | \n" + "|-----------| \n" + "| " + squareBoard[6] + " | " + squareBoard[7] + " | "
+				+ squareBoard[8] + " | \n" + "/---|---|---\\ \n";
 		log.info("Game board : " + createBoard);
 		return createBoard;
 	}
@@ -55,12 +58,12 @@ public class TicTacToeService {
 					in.next();
 					continue;
 				}
-				if (boardOfGame[numInput - 1].equals(String.valueOf(numInput))) {
-					boardOfGame[numInput - 1] = turnOfPlayer;
-					if (turnOfPlayer.equals("X")) {
-						turnOfPlayer = "O";
+				if (squareBoard[numInput - 1].equals(String.valueOf(numInput))) {
+					squareBoard[numInput - 1] = turnOfPlayer;
+					if (turnOfPlayer.equals(PLAYER_X)) {
+						turnOfPlayer = PLAYER_O;
 					} else {
-						turnOfPlayer = "X";
+						turnOfPlayer = PLAYER_X;
 					}
 					getDisplayBoard();
 					winStatusInfo = checkPossibleWaysOfWinningAndGetStatus();
@@ -69,7 +72,7 @@ public class TicTacToeService {
 					continue;
 				}
 			}
-			if (winStatusInfo.equalsIgnoreCase("draw")) {
+			if (winStatusInfo.equalsIgnoreCase(STATUS_DRAW)) {
 				playerStatus = "Hi Guys, Its a draw!, Enjoy the game and try again.";
 				log.info("Hi Guys, Its a draw!, Enjoy the game and try again.");
 			} else {
@@ -90,42 +93,42 @@ public class TicTacToeService {
 			String line = "";
 			switch (a) {
 			case 0:
-				line = boardOfGame[0] + boardOfGame[1] + boardOfGame[2];
+				line = squareBoard[0] + squareBoard[1] + squareBoard[2];
 				break;
 			case 1:
-				line = boardOfGame[3] + boardOfGame[4] + boardOfGame[5];
+				line = squareBoard[3] + squareBoard[4] + squareBoard[5];
 				break;
 			case 2:
-				line = boardOfGame[6] + boardOfGame[7] + boardOfGame[8];
+				line = squareBoard[6] + squareBoard[7] + squareBoard[8];
 				break;
 			case 3:
-				line = boardOfGame[0] + boardOfGame[3] + boardOfGame[6];
+				line = squareBoard[0] + squareBoard[3] + squareBoard[6];
 				break;
 			case 4:
-				line = boardOfGame[1] + boardOfGame[4] + boardOfGame[7];
+				line = squareBoard[1] + squareBoard[4] + squareBoard[7];
 				break;
 			case 5:
-				line = boardOfGame[2] + boardOfGame[5] + boardOfGame[8];
+				line = squareBoard[2] + squareBoard[5] + squareBoard[8];
 				break;
 			case 6:
-				line = boardOfGame[0] + boardOfGame[4] + boardOfGame[8];
+				line = squareBoard[0] + squareBoard[4] + squareBoard[8];
 				break;
 			case 7:
-				line = boardOfGame[2] + boardOfGame[4] + boardOfGame[6];
+				line = squareBoard[2] + squareBoard[4] + squareBoard[6];
 				break;
 			}
 			if (line != null && line.equals("XXX")) {
-				return "X";
+				return PLAYER_X;
 			} else if (line.equals("OOO")) {
-				return "O";
+				return PLAYER_O;
 			}
 		}
 
 		for (int a = 0; a < 9; a++) {
-			if (Arrays.asList(boardOfGame).contains(String.valueOf(a + 1))) {
+			if (Arrays.asList(squareBoard).contains(String.valueOf(a + 1))) {
 				break;
 			} else if (a == 8)
-				return "draw";
+				return STATUS_DRAW;
 		}
 
 		log.info(turnOfPlayer + "'s turn; enter a slot number to place " + turnOfPlayer + " in:");
